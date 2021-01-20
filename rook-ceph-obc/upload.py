e39a22d41ad921227c07943cd8c74aed6a20d4e0
Shon Paz <spaz@redhat.com>
2021-01-20 19:11:46 +0200

    added data eng demo

--- rook-ceph-obc/upload.py
+++ rook-ceph-obc/upload.py
@@ -10,13 +10,13 @@ access_key = os.environ.get('AWS_ACCESS_KEY_ID')
 secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 # builds the endpoint url 
-endpoint_url = 'http://' + str(bucket_host) + ":" + str(bucket_port)
+endpoint_url = 'https://' + str(bucket_host) + ":" + str(bucket_port)
 
 # creates a S3 connection 
 connection = boto3.client('s3',
                            endpoint_url=endpoint_url,
                            aws_access_key_id=access_key,
-                           aws_secret_access_key=secret_key)
+                           aws_secret_access_key=secret_key, verify=False)
 
 # Upload an object to that bucket, will fail if OBC wasn't successful
 with open('/etc/hosts', 'rb') as data:
