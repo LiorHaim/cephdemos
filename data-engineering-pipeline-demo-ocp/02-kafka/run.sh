530a8c6b087e40408dc103be8d210059ae85797e
root <root@registry.spaz.local>
2021-01-31 13:13:10 +0200

    final demo changes

--- data-engineering-pipeline-demo-ocp/02-kafka/run.sh
+++ data-engineering-pipeline-demo-ocp/02-kafka/run.sh
@@ -1,7 +1,7 @@
 oc apply -f kafka.yaml
-sleep 60
+sleep 90
 oc apply -f topics.yaml
 oc apply -f kafkaconnect.yaml
-sleep 30
+sleep 60
 oc apply -f kafkaconnector-s3.yaml
 
--- data-engineering-pipeline-demo-ocp/04-presto/run.sh
+++ data-engineering-pipeline-demo-ocp/04-presto/run.sh
@@ -1,4 +1,4 @@
 oc create secret generic aws-secret --from-literal=AWS_ACCESS_KEY_ID="HC8V2PT7HX8ZFS8NQ37R" --from-literal=AWS_SECRET_ACCESS_KEY="Y6CENKXozDDikJHQgkbLFM38muKBnmWBsAA1DXyU"
 oc apply -f postgres.yaml
-sleep 30
+sleep 60
 oc apply -f presto.yaml
