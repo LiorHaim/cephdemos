3db9698697d8d0c4f435d15c60523c20fd562cab
Shon Paz <shonpaz123@gmail.com>
2022-05-07 14:18:51 +0300

    added pro/con configs

---
+++ containerize-kafka-clients/producer/producer.py
@@ -0,0 +1,35 @@
+from kafka import KafkaProducer
+from json import dumps
+import argparse
+import time
+import logging
+import sys 
+
+Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(
+                    stream = sys.stdout,
+                    filemode = "w",
+                    format = Log_Format,
+                    level = logging.INFO)
+
+logger = logging.getLogger()
+
+
+parser = argparse.ArgumentParser() 
+parser.add_argument('-b', '--bootstrap-servers', help="Bootstrap servers to use from Kafka cluster",
+                            required=True)
+parser.add_argument('-t', '--topic', help="Topic to use from Kafka cluster",
+                            required=True)
+args = parser.parse_args()
+
+print(args.bootstrap_servers)
+producer = KafkaProducer(bootstrap_servers=[args.bootstrap_servers],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
+for e in range(10):
+    data = {'number' : e}
+    logger.info(f"Writing document {data} to Kafka cluster...")
+    producer.send(args.topic, value=data)
+    time.sleep(1)
+
